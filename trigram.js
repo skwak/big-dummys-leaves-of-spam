2ffa5f511096c010985beb97bee759de56ad101f
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-12-13 19:35:03 -0800

    add big dummies text, split into trigrams

--- trigram.js
+++ trigram.js
@@ -27,31 +27,39 @@ var trigram = function() {
   //     });
   //   })();
 
+  // (function() {
+  //   fs.readFile('./text/pg30254.txt', 'utf8', function(err, data) {
+  //     if (err) console.log(err);
+  //
+  //     Algorithmia.client(process.env.ALGORITHMIA_KEY)
+  //       .algo('algo://StanfordNLP/SentenceSplit/0.1.0')
+  //       .pipe(data)
+  //       .then(function(response) {
+  //         fs.writeFile('./text/romance-trigram.txt', response.get(), function(err, response) {
+  //           if (err) console.log(response.error);
+  //         });
+  //       });
+  //
+  //   });
+  // })();
+
   (function() {
-    fs.readFile('./text/pg30254.txt', 'utf8', function(err, data) {
+    fs.readFile('./text/big-dummies.txt', 'utf8', function(err, data) {
       if (err) console.log(err);
 
       Algorithmia.client(process.env.ALGORITHMIA_KEY)
         .algo('algo://StanfordNLP/SentenceSplit/0.1.0')
         .pipe(data)
         .then(function(response) {
-          fs.writeFile('./text/romance-trigram.txt', response.get(), function(err, response) {
+          fs.writeFile('./text/big-dummies-trigram.txt', response.get(), function(err, response) {
             if (err) console.log(response.error);
           });
         });
 
     });
   })();
-    // Algorithmia.client(process.env.ALGORITHMIA_KEY)
-    //   .algo('algo://StanfordNLP/SentenceSplit/0.1.0')
-    //   .pipe(input)
-    //
-    //   .then(function(response) {
-    //     fs.writeFile('./text/leaves-trigram.txt', response.get(), function(err, response) {
-    //       if (err) console.log(response.error);
-    //     });
-    //   });
-    // })();
+
+
 
 
 }

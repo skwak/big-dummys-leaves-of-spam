a57e6fe9db2f0af60f18982ebe4042b8c605fd4a
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2016-01-03 16:45:51 -0800

    add those dang <br> tags

--- public/javascripts/text.js
+++ public/javascripts/text.js
@@ -1,1 +1,6 @@
-// $.get('/').done(function(data){ alert(data["hello"]); });
+$.get('/').done(
+  function checkForWhiteSpace(){
+    var text = $('#spammy-text').html();
+    var newText = text.replace(/\n\n/g, '<br><br>');
+    $('#spammy-text').html(newText);
+  });
